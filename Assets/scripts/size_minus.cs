156ea55fab5dc10e6cea102aa4fb1757ca3db5d0
damakoo <d_kodama@cyber.t.u-tokyo.ac.jp>
2021-05-06 15:03:50 +0900

    first

---
+++ Assets/scripts/size_minus.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class size_minus : MonoBehaviour
+{
+    [SerializeField] sizechange _sizechange;
+    [SerializeField] Text _text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (!_sizechange.pressed_minus)
+        {
+            _sizechange.pressed_minus = true;
+            if (_sizechange.fontsize > 0.001f)
+            {
+                _sizechange.fontsize -= 0.001f;
+            }
+            _text.text = (Mathf.Round(_sizechange.fontsize * 1000) / 10).ToString("f1");
+            Invoke("resetpress_minus", 0.5f);
+
+        }
+    }
+
+    void resetpress_minus()
+    {
+        _sizechange.pressed_minus = false;
+    }
+}
